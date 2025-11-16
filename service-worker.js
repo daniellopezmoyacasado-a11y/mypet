058e78125df8a377f777e6bcade176bd16c2637c
Dani <moyacasadd@coventry.ac.uk>
2025-11-16 19:39:06 +0000

    icon

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "petgame-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+// Install SW and cache files
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Serve cached files when offline
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
